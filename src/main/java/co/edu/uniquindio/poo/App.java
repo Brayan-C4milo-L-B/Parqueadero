740ee5056cd6b5fdc6a51cb127eb752ef175fd7e
Camilo Londoño <bclondonob@uqvirtual.edu.co>
2025-05-26 23:54:51 -0500

    Actulizacion del main

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -2,4 +2,28 @@ package co.edu.uniquindio.poo;
 
 public class App {
 
+    public static void App(String[] args) {
+
+        Vehiculo moto = new Vehiculo(null, null, null, 0);
+
+        Vehiculo camion = new Vehiculo(null, null, null, 0);
+
+        Vehiculo automovil = new Vehiculo(null, null, null, 0);
+
+        vehiculo.add(moto);
+
+        vehiculo.add(camion);
+
+        vehiculo.add(automovil);
+
+        vehiculo.removed(moto);
+
+        vehiculo.removed(camion);
+
+        vehiculo.removed(automovil);
+
+        vehiculo.verificarVehiculo();
+
+    }
+
 }
